ee6f52c93fefb8ad65d2e289ddc4b916231b368d
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-06-04 17:41:12 +0900

    feat) 부분합 커밋

--- ch03_data_structure/solve/49_11660_range_sum_2_sol.cpp
+++ ch03_data_structure/solve/49_11660_range_sum_2_sol.cpp
@@ -4,81 +4,31 @@
 using namespace std;
 int n, m;
 
-void printV(vector<vector<int>> &arr){
-    cout << "####" << '\n';
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << arr[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    
-}
-
-// void printI(int i){
-//     cout << "i :" << i << '\n';
-// }
-
-void minusI(int & i){
-    i--;
-    //printI(i);
-}
-
-
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    vector<vector<int>> v, s;
-
+    int n, m;
     cin >> n >> m;
-    v.resize(n, vector<int>(n));
-    s.resize(n, vector<int>(n));
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> v[i][j];
+    vector<vector<int>> a(n+1, vector<int>(n+1,0));
+    vector<vector<int>> d(n+1, vector<int>(n+1,0));
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> a[i][j];
+            d[i][j] = d[i][j-1] + d[i-1][j] - d[i-1][j-1] + a[i][j];
         }
     }
-    // printV(v);
-    
-    // D[i][j] = D[i][j-1] + D[i-1][j] -D[i-1][j-1] + A[i][j]
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            int prevY = (i > 0) ? s[i-1][j] : 0;
-            int prevX = (j > 0) ? s[i][j-1] : 0;
-            int prevXY = (i > 0 && j > 0) ? s[i-1][j-1] : 0;
-            s[i][j] = v[i][j] + prevX + prevY - prevXY;
+
+    for(int j = 0; j < m; j++){
+            int y1, x1, x2, y2;
+            cin >> y1 >> x1 >> y2 >> x2;
+
+            int result = d[y2][x2] - d[y1-1][x2] - d[y2][x1-1] + d[y1-1][x1-1];
+            cout << result << '\n';
         }
-    }
-    //printV(s);
-
-    for(int i = 0; i < m; i++){
-        int sY, sX, eY, eX;
-        int ret = 0;
-        cin >> sY >> sX >> eY >> eX;
-        // 1 1 2 3
-        minusI(sY);
-        minusI(sX);
-        minusI(eY);
-        minusI(eX);
-
-        // result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
-        if(sY == eY && sX == eX){
-            ret = v[eY][eX];
-        }else{
-            int prevX = sX < 1 ? 0 : s[eY][sX-1];
-            int prevY = sY < 1 ? 0 : s[sY-1][eX];
-            int prevXY = sX < 1 || sY < 1 ? 0 : v[sY - 1][sX-1];
-            ret = s[eY][eX] - prevX - prevY + prevXY;
-        }
-        
-        cout << ret << '\n';
-    }
-
-    
 
     return 0;
 }
