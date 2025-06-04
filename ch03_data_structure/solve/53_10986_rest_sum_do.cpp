02859897ff43c187b891425373f5259d98a12573
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-06-04 17:57:19 +0900

    feat) 나머지 구분 합 실습 중

---
+++ ch03_data_structure/solve/53_10986_rest_sum_do.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a = vector<int>(n);
+    vector<int> d = vector<int>(n);
+    
+    return 0;
+}
