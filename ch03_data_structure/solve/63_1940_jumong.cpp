b98df11032a51a4c1b660f10612c80e2c4997551
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-06-09 17:57:08 +0900

    feat) 주몽 문제 풀이

--- ch03_data_structure/solve/63_1940_jumong.cpp
+++ ch03_data_structure/solve/63_1940_jumong.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>  // sort
 
 using namespace std;
 
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cout.tie(0);
 
     int n, m;
     cin >> n >> m;
-    vector<int> v;
-    v.assign(n,0);
-    
-    for(int i = 0; i < i ++){
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
 
-    
+    sort(v.begin(), v.end());  // 오름차순 정렬
+
+    int s = 0, e = n - 1;
+    int cnt = 0;
+
+    while (s < e) {
+        int sum = v[s] + v[e];
+        if (sum == m) {
+            cnt++;
+            s++;
+            e--;
+        } else if (sum < m) {
+            s++;
+        } else {  // sum > m
+            e--;
+        }
+    }
+
+    cout << cnt << '\n';
 
     return 0;
 }
