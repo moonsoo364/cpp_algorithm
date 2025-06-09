81b26dc77ec41d125f1cb9a5a2cbbb225392f783
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-06-09 17:46:40 +0900

    feat) 주몽 문제 세팅

---
+++ ch03_data_structure/solve/63_1940_jumong.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> v;
+    v.assign(n,0);
+    
+    for(int i = 0; i < i ++){
+        cin >> v[i];
+    }
+
+    
+
+    return 0;
+}
