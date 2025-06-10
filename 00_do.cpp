00ab99189bbda41d62ac3906dc7ae1bdefe55306
Moonsoo <aptx15hibr@naver.com>
2025-06-11 08:10:13 +0900

    feat) pq 문제 풀이 중

--- 00_do.cpp
+++ 00_do.cpp
@@ -1,42 +1,22 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 
 using namespace std;
 
+struct Comp{
+    bool operator()(int x, int y){
+        if(abs(x) == abs(y)) x < y;
+        return abs(x) < abs(y);
+    }
+};
+
 int main(){
-    cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
     int n;
     cin >> n;
-    vector<int> v(n);
-
-    for(int i = 0; i < n; i++){
-        cin >> v[i];
-    }
-    sort(v.begin(),v.end());
-
-    int cnt=0;
-   for(int i = 0; i<n;i++){
-    int t = v[i];
-    int s=0, e=n-1;
-    while( s < e){
-        if(s == i){s++; continue;}
-        if(e == i){e--; continue;}
-        long long sum = (long long)v[s] + v[e];
-        if(t == sum){
-            cnt++;
-            break;
-        }else if(t > sum){
-            s++;
-        }else{
-            e--;
-        }
-     }
-   }
-    cout << cnt << '\n';
+    priority_queue<int, vector<int,Comp>> pq;
     
-    
-
 
     return 0;
 }
